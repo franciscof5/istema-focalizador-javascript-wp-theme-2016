6371092ca9e9ed0389b9e007e475e9181c251ba6
Francisco Mat <francisco@f5sites.com>
2018-08-16 05:18:23 -0300

    exclusive content title

--- part-closed.php
+++ part-closed.php
@@ -1,3 +1,6 @@
+<script language="javascript">
+    document.title = "Pomodoros - Exclusive Content";
+</script>
 <div class="content_nosidebar col-xs-12 col-sm-6" style="width: 104%;left: -2%;" >
 	<h3  style="margin:50px;">Acesso restrito</h3>
 	<p class="bg-danger" style="margin:50px; font-size: 14px; padding:20px;"><a href="#" class="abrir_login">Acesse sua conta</a> para ver o conteúdo</p>
